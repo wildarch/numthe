3f2781017bbc63af6103369d5a78bba100c49cbe
wildarch <daandegraaf9@gmail.com>
2017-09-07 11:52:43 +0200

    Skeleton for InputParser

---
+++ src/InputParser.java
@@ -0,0 +1,15 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Created by daan on 07/09/17.
+ */
+public class InputParser {
+    public InputParser() {
+    }
+
+    public static InputParser parse(InputStream is) {
+        Scanner scanner = new Scanner(is);
+        scanner.skip("")
+    }
+}
