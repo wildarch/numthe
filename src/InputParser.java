bfd44976148977e98fbb9d4c53d13b8bad0ac2cf
wildarch <daandegraaf9@gmail.com>
2017-09-07 18:22:34 +0200

    Working implementation of Parser, rough around the edges

--- src/InputParser.java
+++ src/InputParser.java
@@ -1,15 +1,89 @@
-import java.io.InputStream;
+import com.sun.org.apache.xpath.internal.operations.Mult;
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
- * Created by daan on 07/09/17.
+ * Created by Daan de Graaf on 07/09/17.
  */
 public class InputParser {
-    public InputParser() {
+
+    private int radix;
+    private Operation operation;
+    private String x;
+    private String y;
+    private String answer;
+
+    public InputParser(Stream<String> input) throws ParseException {
+        input
+            .map(String::trim)
+            .filter(s -> !s.isEmpty())
+            .map(s -> s.split(" "))
+            .forEach(parts -> {
+                switch(parts[0]) {
+                    case "#":
+                        if(parts.length > 2 && parts[1].equals("[answer]")) {
+                            answer = parts[2];
+                        }
+                        // Do nothing for ordinary comments
+                        break;
+                    case "[radix]":
+                        radix = Integer.parseInt(parts[1]);
+                        break;
+                    case "[add]":
+                        operation = Operation.Add;
+                        break;
+                    case "[subtract]":
+                        operation = Operation.Subtract;
+                        break;
+                    case "[multiply]":
+                        operation = Operation.Multiply;
+                        break;
+                    case "[karatsuba]":
+                        operation = Operation.Karatsuba;
+                        break;
+                    case "[x]":
+                        x = parts[1];
+                        break;
+                    case "[y]":
+                        y = parts[1];
+                        break;
+                    default:
+                        throw new ParseException("Could not parse line: `" + String.join(" ", parts) + "`");
+                }
+            });
     }
 
-    public static InputParser parse(InputStream is) {
-        Scanner scanner = new Scanner(is);
-        scanner.skip("")
+    @Override
+    public String toString() {
+        String s = "Parsed data:\n";
+        s += "radix: " + radix + "\n";
+        s += "operation: " + operation + "\n";
+        s += "x: " + x + "\n";
+        s += "y:" + y + "\n";
+        if (answer != null) {
+            s += "answer: " + answer + "\n";
+        }
+        return s;
+    }
+
+    public static class ParseException extends RuntimeException {
+        private String message;
+
+        public ParseException(String msg) {
+            message = msg;
+        }
+    }
+
+    public static enum Operation {
+        Add,
+        Subtract,
+        Multiply,
+        Karatsuba
     }
 }
--- src/NumberTheory.java
+++ src/NumberTheory.java
@@ -1,9 +1,25 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 /**
  * @author Ruben Schellekens
  */
 public class NumberTheory {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InputStream stream = null;
+        try {
+            Stream<String> input = Files.lines(Paths.get("example-test.txt"));
+            InputParser parser = new InputParser(input);
+            System.out.println(parser);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InputParser.ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
