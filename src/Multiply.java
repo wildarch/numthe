4bee3cf4cfab0070ad2e3478bc9411082941a3dc
codemaster97 <erik_97@live.nl>
2017-09-14 13:35:45 +0200

    Multiply working

--- src/Multiply.java
+++ src/Multiply.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 /**
  * Class that implements (naive) multiplication
@@ -21,6 +23,9 @@ public class Multiply {
         base = x.getBase();
 
         solve();
+        //result.add(6);
+        //sign = Sign.POSITIVE;
+        //base = 10;
         //Collections.reverse(result); // Switch around because we scanned from right to left
         return new LargeNumber(base, sign, result);
     }
@@ -28,19 +33,27 @@ public class Multiply {
     /**
      * Solves the multiplication with the algorithm from lecture notes
      */
+
     private void solve() {
-        for (int i = 0; i < x.size() * y.size(); i++) {
+        System.out.println("x_size " + x.size());
+        System.out.println("y_size " + y.size());
+        for (int i = 0; i <= x.size() * y.size(); i++) {
             result.add(0);
+            System.out.println("added 0");
         }
 
-        for (int i = 0; i < x.size() - 1; i++) {
+        for (int i = 0; i <= x.size() - 1; i++) {
             int carry = 0;
-            for (int j = 0; j < y.size() - 1; j++) {
+            for (int j = 0; j <= y.size() - 1; j++) {
                 int t = result.get(result.size() - 1 - (i + j)) + x.get(x.size() - 1 - i) * y.get(y.size() - 1 - j) + carry;
+                System.out.println("t: " + t);
                 carry = t / base;
+                System.out.println("carry: " + carry);
                 result.set(result.size() - 1 - (i + j), t - carry * base);
+                System.out.println("On index " + (result.size() - 1 - (i + j)) + " we set " + (t - carry * base));
+                System.out.println("Index " + (result.size() - 1 - (i + j)) + " has value " + result.get(result.size() - 2 - (i + j)));
             }
-            result.set(i + y.size(), carry);
+            result.set(result.size() - i - y.size() - 1, carry);
         }
 
         if (x.getSign() == y.getSign()) {
