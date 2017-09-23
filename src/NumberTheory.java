30226c7618672863b6d71bf7c12da4a68042afac
Thomas Brocken <thomasbrocken@live.nl>
2017-09-23 20:54:29 +0200

    Add option for filename

--- src/NumberTheory.java
+++ src/NumberTheory.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.InputMismatchException;
 import java.util.stream.Stream;
 
 
@@ -8,10 +11,22 @@ import java.util.stream.Stream;
 public class NumberTheory {
 
     public static void main(String[] args) {
-        try {
-            //Stream<String> input = Files.lines(Paths.get("example-karatsuba.txt"));
+        Stream<String> input = null;
+        if (args.length == 2 && args[0].equals("-f")) {
+            String filename = args[1];
+            try {
+            input = Files.lines(Paths.get(filename)); }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (args.length != 0) {
+            throw new InputMismatchException("Invalid arguments");
+        } else if (args.length == 0) {
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            Stream<String> input = in.lines().limit(4);
+            input = in.lines().limit(4);
+        }
+        try {
             InputParser parser = new InputParser(input);
             System.out.println(parser);
             LargeNumber answer = parser.getOperation().execute(parser.getX(), parser.getY());
